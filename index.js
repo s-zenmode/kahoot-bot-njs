cf806db10ed28a4907bdf2b4af8bfc9557f997c0
s-zenmode <83439414+s-zenmode@users.noreply.github.com>
2021-09-26 22:44:51 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var Kahoot = require("kahoot.js-updated");
+var kahoots = [];
+var pin = "3470515";
+var name = "KahootBot";
+var bot_count = 230;
+for (var i = 0; i < bot_count; i++) {
+    kahoots.push(new Kahoot);
+    kahoots[i].join(pin, name+" "+String(i)).catch(error => {
+        console.log("join failed " + error.description + " " + error.status)
+    });
+    kahoots[i].on("Joined", () => {
+        console.log("successfully joined game")
+    });
+    kahoots[i].on("QuestionStart", (question) => {
+        question.answer(
+            Math.floor(
+                Math.random() * question.quizQuestionAnswers[question.questionIndex]
+            ) + 0
+        );
+    });
+    kahoots[i].on("Disconnect", (reason) => {
+        console.log("disconnected because " + reason);
+    });
+}
---
+++ readme.md
@@ -0,0 +1,20 @@
+#kahoot-bot-njs
+
+kahoot bot in node.js
+
+requirements:
+
+nodejs, and install packahe kahoot.js-updated `npm i kahoot.js-updated`
+
+original tutorial by [this guy](https://www.youtube.com/watch?v=JLi_qnqXYPw&t=150s)
+
+
+HOW TO SET UP:
+
+1. install kahoot.js-updated (`npm i kahoot.js-updated`)
+
+2. CHANGE THE PIN NUMBER AND USERNAME INSIDE INDEX.JS FILE!!!!! (IMPORTANT)
+
+3. node index.js
+
+4. profit
